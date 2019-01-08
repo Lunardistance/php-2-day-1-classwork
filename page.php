f894db549d6844679d558f695e90158db999c6ad
Gabriel Luciano <Gluciano@Gabriels-MacBook-Pro.local>
2019-01-07 21:33:23 -0500

    second commit

--- page.php
+++ page.php
@@ -4,3 +4,10 @@ $data = $_GET["e"];
 $array = explode(",", $data);
 
 var_dump($array);
+
+$displayed = "<ul>";
+foreach ($array as $value){
+   echo "<li>".$value."</li>";
+}
+
+$displayed .= "</ul>";
